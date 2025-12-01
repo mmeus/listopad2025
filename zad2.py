0b2cfc146b11c757bb457e26e639774f1abfac9e
mmeus <maxiking156274@gmail.com>
2025-12-01 12:33:53 +0100

    SasAS

---
+++ zad2.py
@@ -0,0 +1,34 @@
+#zad2.3
+def horner(n, a, A):
+    wynik = A[0]
+    for i in range(n - 1, -1, -1): #od n-1 do 0 co -1(malejąco co 1)
+        wynik = wynik * a + A[n - 1]
+    return wynik
+
+A = [2, 5, 7, 9]
+n = len(A) - 1
+print(horner(n, 1, A))
+
+#zad2.4
+dane = [list(map(int, w.split()))[1:] for w in open('wielomiany.txt')]
+
+max_r = 0
+max_w = []
+
+for w in dane:
+    n = len(w) - 1
+    l_mn_tr = n * (n + 1) / 2
+    l_mn_hr = n
+    r = l_mn_tr - l_mn_hr
+    if r == max_r:
+        max_w.append(w)
+    elif r > max_r:
+        max_r = r
+        max_w = [w]
+
+
+#max_w = max_w.insert(0, len(max_w) - 1)
+for w in max_w:
+    w = [len(max_w) - 1] + w
+    print(w)
+print(max_r)
